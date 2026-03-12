97cfd266ee233a8631d1894ca684eceeabe6c8f4
Mansolo-21 <solomonjohari@gmail.com>
2026-03-12 10:04:12 +0300

    more

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
+from django.contrib.auth import views as auth_views 
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('',include('dashboard.urls')),
     path('accounts/',include('accounts.urls')),
     path('assignments/',include('assignments.urls')),
