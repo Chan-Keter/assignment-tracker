4c7b69fc504556cc3679cc94633a03e2dbf534ff
Chan-Keter <chantelleketer@gmail.com>
2026-03-12 16:03:18 +0300

    Save work before pulling main

--- tracker/tracker/urls.py
+++ tracker/tracker/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    
     path('',include('dashboard.urls')),
     path('accounts/',include('accounts.urls')),
     path('assignments/',include('assignments.urls')),
